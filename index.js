52460c8b671ef67c52181c41b3aa817ef8872e1b
Ashiqur <ashiqur2812@gmail.com>
2024-12-25 15:48:49 +0600

    fetched individual email data

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.use(express());
 app.use(cors());
+app.use(express.json());
 
-// food-kingdom
-// 2quxAh0ClSeJSOg8
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yt5iw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,11 +24,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
 
+        const foodsCollection = client.db('food-db').collection('foods');
 
+        // get all foods from db 
+
+        app.get('/foods', async (req, res) => {
+            const result = await foodsCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/foods/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { 'buyer.email': email };
+            console.log(filter);
+            const result = await foodsCollection.find(filter).toArray();
+            res.send(result);
+        });
+
+        app.post('/add-foods', async (req, res) => {
+            const food = req.body;
+            const result = await foodsCollection.insertOne(food);
+            console.log(result);
+            res.send(result);
+        });
 
 
         await client.db("admin").command({ ping: 1 });
